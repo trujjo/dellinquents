dd540c06e28a352a31394fa9eab4d7265f5f09d7
Jon Trujillo <JonTrujillo@UTexas.edu>
2025-01-24 15:29:57 -0600

    Update auth_check.js

--- auth_check.js
+++ auth_check.js
@@ -1,6 +1,11 @@
 // auth_check.js
 (function() {
-    if (!localStorage.getItem('authenticated')) {
+    // Assuming SITE_PASSWORD is available as an environment variable
+    const correctPassword = process.env.SITE_PASSWORD;
+
+    const storedPassword = localStorage.getItem('password');
+
+    if (!storedPassword || storedPassword !== correctPassword) {
         window.location.href = '/login.html';
     }
 })();
